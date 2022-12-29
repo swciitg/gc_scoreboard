464efac456edb23c09cf1a5d8ee528fe0307a7c7
Hareesh nandigrama <85237630+Hareesh-Nandigrama@users.noreply.github.com>
2022-12-29 14:13:50 +0530

    refactor 1

---
+++ packages/scoreboard/lib/src/decorations/card_style.dart
@@ -0,0 +1,27 @@
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/material.dart';
+import '../globals/themes.dart';
+
+final dateWidgetStyle = GoogleFonts.montserrat(
+    fontWeight: FontWeight.w700, fontSize: 16, color: Themes.cardFontColor2);
+
+final cardEventStyle = GoogleFonts.montserrat(
+    fontWeight: FontWeight.w700,
+    fontSize: 16,
+    color: Themes.cardFontColor2);
+
+final cardCategoryStyle = GoogleFonts.montserrat(
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+    color: Themes.bottomNavFontColor);
+
+
+final cardStageStyle1 = GoogleFonts.montserrat(
+    fontWeight: FontWeight.w600,
+    fontSize: 12,
+    color: Themes.cardFontColor3);
+
+final cardStageStyle2 = GoogleFonts.montserrat(
+    fontWeight: FontWeight.w600,
+    fontSize: 12,
+    color: Themes.primaryColor);
