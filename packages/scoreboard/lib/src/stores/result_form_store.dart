0da7defd6915e4884f7625a21aae0a629b500fca
Priyanshu Srivastava <priyanshusrivastava42@gmail.com>
2022-12-29 14:14:40 +0530

    used setstate instead of mobx in add results

---
+++ packages/scoreboard/lib/src/stores/result_form_store.dart
@@ -0,0 +1,44 @@
+import 'package:scoreboard/src/models/nullable_result_model.dart';
+
+class ResultForm {
+  static List<NullableResultModel>? resultFields = [NullableResultModel(position: 1)] ;
+
+  static void addTie(int position) {
+    resultFields?.add(NullableResultModel(position: position));
+    resultFields?.sort();
+  }
+
+
+  static void removePosition(int index) {
+    resultFields?.removeAt(index);
+    resultFields?.sort();
+  }
+
+  static int get numResults => resultFields?.length ?? 0;
+
+
+  static void addNewPosition(int? value) {
+    if (value == null) return;
+    resultFields?.add(NullableResultModel(position: value + 1));
+    resultFields?.sort();
+  }
+
+  static int numResultsWithPosition(int position) {
+    if (resultFields == null) return 0;
+    return resultFields!
+        .where((element) => element.position == position)
+        .length;
+  }
+
+  static bool displayAddTieIcon(int resultFieldIndex) =>
+      numResultsWithPosition(resultFields![resultFieldIndex].position) < 2;
+
+  static bool displayRemoveTieIcon(int resultFieldIndex) =>
+      numResultsWithPosition(resultFields![resultFieldIndex].position) == 2 &&
+      resultFieldIndex != 0 &&
+      resultFields![resultFieldIndex].position ==
+          resultFields![resultFieldIndex - 1].position;
+
+  static bool displayAddPosition(int resultFieldIndex) =>
+      resultFieldIndex == (numResults - 1);
+}
