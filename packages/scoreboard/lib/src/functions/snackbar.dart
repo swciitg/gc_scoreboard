f0145d8abb266978b6759f131f63c326e9d44aef
mauryakshitij <kkmaurya2004@gmail.com>
2023-01-10 19:30:42 +0530

    Edit Snackbar

--- packages/scoreboard/lib/src/functions/snackbar.dart
+++ packages/scoreboard/lib/src/functions/snackbar.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 void showSnackBar(BuildContext buildContext,String message){
-  ScaffoldMessenger.of(buildContext).showSnackBar(SnackBar(content: Text(message)));
+  ScaffoldMessenger.of(buildContext).showSnackBar(SnackBar(content: Text(message,style: GoogleFonts.montserrat(),)));
 }
