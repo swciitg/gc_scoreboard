9a9aa7137375f88bbefae5c0a636115967aea01d
Vignesh Rao <vigneshravirao@gmail.com>
2023-01-12 23:59:41 +0530

    Timepicker color changes

--- packages/scoreboard/lib/src/widgets/add_event/timepicker_color.dart
+++ packages/scoreboard/lib/src/widgets/add_event/timepicker_color.dart
@@ -14,46 +14,60 @@ class _TimePickerColorState extends State<TimePickerColor> {
   Widget build(BuildContext context) {
     return Theme(
       data: Theme.of(context).copyWith(
-          timePickerTheme: TimePickerThemeData(
-        backgroundColor: const Color(0xff273141),
-        dayPeriodBorderSide: const BorderSide(color: Color.fromRGBO(91, 146, 227, 1), width: 2),
-        dayPeriodColor: MaterialStateColor.resolveWith((states) =>
-            states.contains(MaterialState.selected)
-                ? Color.fromRGBO(91, 146, 227, 1)
-                : const Color(0xff273141)),
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(8)),
+        timePickerTheme: TimePickerThemeData(
+          backgroundColor: const Color(0xff273141),
+          dayPeriodBorderSide: const BorderSide(
+              color: Color.fromRGBO(91, 146, 227, 1), width: 2),
+          dayPeriodColor: MaterialStateColor.resolveWith((states) =>
+              states.contains(MaterialState.selected)
+                  ? Color.fromRGBO(91, 146, 227, 1)
+                  : const Color(0xff273141)),
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+          ),
+          dayPeriodTextColor: MaterialStateColor.resolveWith((states) =>
+              states.contains(MaterialState.selected)
+                  ? Colors.white
+                  : Colors.blueGrey.shade600),
+          dayPeriodShape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+            side: BorderSide(width: 4),
+          ),
+          hourMinuteColor: MaterialStateColor.resolveWith((states) =>
+              states.contains(MaterialState.selected)
+                  ? Color.fromRGBO(91, 146, 227, 1)
+                  : const Color(0xff2B3E5C)),
+          hourMinuteTextColor: MaterialStateColor.resolveWith((states) =>
+              states.contains(MaterialState.selected)
+                  ? Colors.white
+                  : Colors.white),
+          dialHandColor: Color.fromRGBO(118, 172, 255, 1),
+          dialBackgroundColor: const Color(0xff2B3E5C),
+          hourMinuteTextStyle: GoogleFonts.montserrat(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+          dayPeriodTextStyle:
+              GoogleFonts.montserrat(fontSize: 12, fontWeight: FontWeight.bold),
+          helpTextStyle: GoogleFonts.montserrat(
+              fontSize: 12, fontWeight: FontWeight.bold, color: Colors.white),
+          dialTextColor: MaterialStateColor.resolveWith((states) =>
+              states.contains(MaterialState.selected)
+                  ? Colors.black
+                  : Colors.white),
+          entryModeIconColor: Colors.blueGrey.shade600,
         ),
-        dayPeriodTextColor: MaterialStateColor.resolveWith((states) =>
-            states.contains(MaterialState.selected)
-                ? Colors.white
-                : Colors.blueGrey.shade600),
-        dayPeriodShape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(8)),
-          side: BorderSide(width: 4),
+        textTheme: TextTheme(
+          caption: GoogleFonts.montserrat(), // days
         ),
-        hourMinuteColor: MaterialStateColor.resolveWith((states) =>
-            states.contains(MaterialState.selected)
-                ? Color.fromRGBO(91, 146, 227, 1)
-                : const Color(0xff2B3E5C)),
-        hourMinuteTextColor: MaterialStateColor.resolveWith((states) =>
-            states.contains(MaterialState.selected)
-                ? Colors.white
-                : Colors.white),
-        dialHandColor: Color.fromRGBO(118, 172, 255, 1),
-        dialBackgroundColor: const Color(0xff2B3E5C),
-        hourMinuteTextStyle:
-            GoogleFonts.montserrat(fontSize: 20, fontWeight: FontWeight.bold,),
-        dayPeriodTextStyle:
-            GoogleFonts.montserrat(fontSize: 12, fontWeight: FontWeight.bold),
-        helpTextStyle: GoogleFonts.montserrat(
-            fontSize: 12, fontWeight: FontWeight.bold, color: Colors.white),
-        dialTextColor: MaterialStateColor.resolveWith((states) =>
-            states.contains(MaterialState.selected)
-                ? Colors.black
-                : Colors.white),
-        entryModeIconColor: Colors.blueGrey.shade600,
-      )),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+              backgroundColor: const Color(0xff273141), // button
+              foregroundColor: Color.fromRGBO(118, 172, 255, 1),
+              elevation: 0,
+              textStyle: GoogleFonts.montserrat()),
+        ),
+      ),
       child: MediaQuery(
           data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: false),
           // If you want 24-Hour format, just change alwaysUse24HourFormat to true or remove all the builder argument
