59778952d05954c74e66ba816391a2a26f7fc400
Chanchal Yadav <chanchalyadav272@gmail.com>
2023-01-14 16:02:41 +0530

    fixed popmenu

--- packages/scoreboard/lib/src/widgets/cards/results_card.dart
+++ packages/scoreboard/lib/src/widgets/cards/results_card.dart
@@ -33,150 +33,148 @@ class _ResultsCardState extends State<ResultsCard> {
   Widget build(BuildContext context) {
     var commonStore = context.read<CommonStore>();
     return Observer(builder: (context) {
+      ;
+
       return Padding(
         padding: const EdgeInsets.all(8.0),
         child: ConstrainedBox(
-          constraints: const BoxConstraints(minHeight: 186),
-          child: PopupMenu(
-                  eventModel: widget.eventModel,
-                  items: commonStore.viewType==ViewType.admin ? popupOptions : [],
-                  child: Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(24),
-                        color: Themes.cardColor2),
-                    child: Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Column(
-                        children: [
-
-                          CardEventDetails(eventModel: widget.eventModel),
-                          SizedBox(
-                            height: 24,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Expanded(
-                                  child: Row(
-                                    children: [
-                                      const Icon(
-                                        Icons.emoji_events_outlined,
-                                        color: Themes.cardFontColor3,
-                                        size: 12,
-                                      ),
-                                      const SizedBox(
-                                        width: 4,
-                                      ),
-                                      Expanded(
-                                        child: Text(
-                                          widget.eventModel.victoryStatement!,
-                                          overflow: TextOverflow.ellipsis,
-                                          style: GoogleFonts.montserrat(
-                                              fontWeight: FontWeight.w600,
-                                              fontSize: 12,
-                                              color: Themes.cardFontColor3),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                GestureDetector(
-                                  onTap: () {
-                                    setState(() {
-                                      isExpanded = !isExpanded;
-                                    });
-                                  },
-                                  child: Container(
-                                    height: 24,
-                                    decoration: BoxDecoration(
-                                        borderRadius:
-                                            BorderRadius.circular(100),
-                                        color: Themes.kGrey),
-                                    width: 64,
-                                    child: Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                          horizontal: 12, vertical: 6),
-                                      child: Row(
-                                        children: [
-                                          Text(
-                                            isExpanded ? 'Less' : 'More',
-                                            style: GoogleFonts.montserrat(
-                                                fontWeight: FontWeight.w500,
-                                                fontSize: 10,
-                                                color: Themes.cardFontColor2),
-                                          ),
-                                          Icon(
-                                            isExpanded
-                                                ? Icons
-                                                    .keyboard_arrow_up_outlined
-                                                : Icons
-                                                    .keyboard_arrow_down_outlined,
-                                            size: 14,
-                                            color: Themes.cardFontColor2,
-                                          )
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                          isExpanded
-                              ? Column(
-                                  children: [
-                                    const Divider(
-                                      height: 32,
-                                      color: Themes.bottomNavHighlightColor,
-                                      thickness: 1,
-                                    ),
-                                    SizedBox(
-                                      height: 12,
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Row(
-                                            children: [
-                                              const SizedBox(
-                                                width: 26,
-                                              ),
-                                              Text(
-                                                'Hostel',
-                                                style: GoogleFonts.montserrat(
-                                                    fontWeight: FontWeight.w600,
-                                                    fontSize: 10,
-                                                    color: Themes
-                                                        .bottomNavFontColor),
-                                              ),
-                                            ],
-                                          ),
-                                          Text(
-                                            'Score',
-                                            style: GoogleFonts.montserrat(
-                                                fontWeight: FontWeight.w600,
-                                                fontSize: 10,
-                                                color:
-                                                    Themes.bottomNavFontColor),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                          vertical: 2),
-                                      child: ExpandedResultsCard(
-                                          eventModel: widget.eventModel),
-                                    )
-                                  ],
-                                )
-                              : Container(),
-                        ],
-                      ),
-                    ),
-                  ),
-                )
-        ),
+            constraints: const BoxConstraints(minHeight: 186),
+            child: commonStore.viewType == ViewType.admin
+                ? PopupMenu(
+                    eventModel: widget.eventModel,
+                    items: popupOptions,
+                    child: ResultCardItems(),
+                  )
+                : ResultCardItems()),
       );
     });
+  }
+
+  Widget ResultCardItems() {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(24), color: Themes.cardColor2),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            CardEventDetails(eventModel: widget.eventModel),
+            SizedBox(
+              height: 24,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Row(
+                      children: [
+                        const Icon(
+                          Icons.emoji_events_outlined,
+                          color: Themes.cardFontColor3,
+                          size: 12,
+                        ),
+                        const SizedBox(
+                          width: 4,
+                        ),
+                        Expanded(
+                          child: Text(
+                            widget.eventModel.victoryStatement!,
+                            overflow: TextOverflow.ellipsis,
+                            style: GoogleFonts.montserrat(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 12,
+                                color: Themes.cardFontColor3),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isExpanded = !isExpanded;
+                      });
+                    },
+                    child: Container(
+                      height: 24,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          color: Themes.kGrey),
+                      width: 64,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 12, vertical: 6),
+                        child: Row(
+                          children: [
+                            Text(
+                              isExpanded ? 'Less' : 'More',
+                              style: GoogleFonts.montserrat(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 10,
+                                  color: Themes.cardFontColor2),
+                            ),
+                            Icon(
+                              isExpanded
+                                  ? Icons.keyboard_arrow_up_outlined
+                                  : Icons.keyboard_arrow_down_outlined,
+                              size: 14,
+                              color: Themes.cardFontColor2,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            isExpanded
+                ? Column(
+                    children: [
+                      const Divider(
+                        height: 32,
+                        color: Themes.bottomNavHighlightColor,
+                        thickness: 1,
+                      ),
+                      SizedBox(
+                        height: 12,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                const SizedBox(
+                                  width: 26,
+                                ),
+                                Text(
+                                  'Hostel',
+                                  style: GoogleFonts.montserrat(
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 10,
+                                      color: Themes.bottomNavFontColor),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              'Score',
+                              style: GoogleFonts.montserrat(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 10,
+                                  color: Themes.bottomNavFontColor),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 2),
+                        child:
+                            ExpandedResultsCard(eventModel: widget.eventModel),
+                      )
+                    ],
+                  )
+                : Container(),
+          ],
+        ),
+      ),
+    );
   }
 }
