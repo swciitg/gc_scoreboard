60ee5fa36e3693d4d13651c9e631b127013546f8
Tanisha <tanishasalvi12@gmail.com>
2023-03-17 02:19:52 +0530

    primary score display

--- packages/scoreboard/lib/src/widgets/cards/manthan_result_card.dart
+++ packages/scoreboard/lib/src/widgets/cards/manthan_result_card.dart
@@ -204,6 +204,132 @@ class _ManthanResultCardState extends State<ManthanResultCard> {
   }
 }
 
+// class HostelsPointsSection extends StatelessWidget {
+//   final eventModel;
+//
+//   HostelsPointsSection({Key? key, required this.eventModel})
+//       : super(key: key);
+//
+//   var length = 0;
+//
+//   void count() {
+//     for(var x in eventModel.results)
+//       {
+//         length++;
+//       }
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     count();
+//     return ConstrainedBox(
+//         constraints: BoxConstraints(
+//           maxHeight: length * 30,
+//         ),
+//         child: ListView.builder(
+//             physics: const NeverScrollableScrollPhysics(),
+//             itemCount: eventModel.results.length,
+//             itemBuilder: (context, index) {
+//               return ConstrainedBox(
+//                 constraints: BoxConstraints(
+//                     maxHeight: 1 * 30),
+//                 child: ListView.builder(
+//                     physics: const NeverScrollableScrollPhysics(),
+//                     itemCount: eventModel.results[index],
+//                     itemBuilder: (context, subIndex) {
+//                       return scoreCardItem(
+//                           index + 1,
+//                           eventModel.results[index][subIndex].hostelName!,
+//                           eventModel.results[index][subIndex].primaryScore!,
+//                           eventModel.results[index][subIndex].secondaryScore);
+//                     }),
+//               );
+//             }));
+//   }
+//
+//
+//   Widget scoreCardItem(int position, String hostelName, String finalScore,
+//       String? secondaryScore) {
+//     final split = secondaryScore?.split(',');
+//
+//     return Padding(
+//       padding: const EdgeInsets.symmetric(vertical: 6),
+//       child: Row(
+//         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//         children: [
+//           SizedBox(
+//             height: 18,
+//             child: Row(
+//               crossAxisAlignment: CrossAxisAlignment.center,
+//               children: [
+//                 Container(
+//                   alignment: Alignment.center,
+//                   width: 16,
+//                   height: 18,
+//                   child: Text(
+//                     '$position',
+//                     style: cardVenueStyle1,
+//                   ),
+//                 ),
+//                 const SizedBox(
+//                   width: 10,
+//                 ),
+//                 SizedBox(
+//                   width: 105,
+//                   child: Text(
+//                     overflow: TextOverflow.visible,
+//                     hostelName,
+//                     style: cardVenueStyle1,
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//           Container(
+//             alignment: Alignment.center,
+//             height: 18,
+//             child: Row(
+//               children: [
+//                 Text(
+//                   finalScore,
+//                   style: cardPostponedStyle,
+//                 ),
+//                 secondaryScore == null
+//                     ? Container()
+//                     : Row(
+//                   children: [
+//                     const SizedBox(
+//                       width: 8,
+//                     ),
+//                     SizedBox(
+//                       width: (split?.length.toDouble())! * 18,
+//                       child: ListView.builder(
+//                           itemCount: split?.length,
+//                           scrollDirection: Axis.horizontal,
+//                           physics: const NeverScrollableScrollPhysics(),
+//                           itemBuilder: (context, index) {
+//                             return Container(
+//                               alignment: Alignment.centerRight,
+//                               width: 18,
+//                               child: Text(
+//                                 split![index],
+//                                 style: cardSecondaryScoreStyle,
+//                               ),
+//                             );
+//                           }),
+//                     ),
+//                   ],
+//                 ),
+//               ],
+//             ),
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+
 class HostelsPointsSection extends StatelessWidget {
   final eventModel;
 
@@ -253,20 +379,9 @@ class HostelsPointsSection extends StatelessWidget {
                   Container(
                     alignment: Alignment.center,
                     height: 18,
-                    child: Row(
-                      children: [
-                        Text("(", style: cardPostponedStyle,),
-                        Text(
-                          eventModel.results[position].primaryScore!.toString(),
-                          style: cardPostponedStyle,
-                        ),
-                        Text(",", style: cardPostponedStyle,),
-                        Text(
-                          eventModel.results[position].secondaryScore!.toString(),
-                          style: cardPostponedStyle,
-                        ),
-                        Text(")", style: cardPostponedStyle,),
-                      ],
+                    child: Text(
+                      eventModel.results[position].primaryScore!.toString(),
+                      style: cardPostponedStyle,
                     ),
                   )
                 ],
